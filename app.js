7b46acd268d7f2e799b4a827d8e0f66172078c16
RehmanSohail1 <111071020+RehmanSohail1@users.noreply.github.com>
2023-12-23 05:26:02 +0500

    complete

---
+++ app.js
@@ -0,0 +1,200 @@
+// alert ("first name")
+// alert ("last name")
+// alert ("number")
+// alert ("phone no")
+// alert ("password")
+
+
+// alert("You're learning JavaScript!");
+// alert("assalam o alaikum");
+// // CHAPTER 2
+
+// var myteam ;
+// myteam=("anus")
+
+// var teamname =("pakistan")
+// alert(teamname);
+
+// var bestman =("charlie")
+// bestman =("anus")
+
+// alert (bestman)
+
+// // CHAPTER 3
+
+// var caseqty ;
+// caseqty = 144;
+
+// var num = 9;
+
+// var sum ;
+// sum = 2+2;
+// alert (sum);
+
+// var merchtotal =100;
+// var shipping =10;
+// var ordertotal = merchtotal+shipping
+// alert (ordertotal);
+
+// var a = 2
+// a= 2+3
+// alert (a)
+
+// CHAPTER 4
+
+// var  productcoast =3.45 ;
+
+// var nameOfBand;
+
+// var legallyRehman = 10;
+
+// var abdulMoiz =5 
+
+// var myName =legal
+// var abdulMoiz =legald
+// // var 12myname =illegal
+// // var 12moiz =illegal
+
+// var remainder =10 % 3 ;
+
+// var num = 20 % 6;
+
+// var largeNum = 1000 * 2000;
+// alert (largeNum);
+
+// var a = 5;
+// var b = 3;
+// var c = 5-3
+// alert (c)
+
+// var a =  18
+// var b = 5
+// var result = a % b;
+// alert (result)
+
+// var multiply = 5 * 3
+// alert (multiply)
+
+// CHAPTER 6
+
+// var a = 1;
+// a= 1+1
+
+// alert (a)
+
+// var a = 100;
+// a= 100-1
+
+// alert (a)
+
+// var x =50;
+// var y = x++;
+
+// alert (y)
+// var x =50;
+// var y = --x;
+
+// alert (y)
+
+// var num1 = 5;
+// var num2  =--num1;
+
+// alert (num2)
+
+// var num1 = 5;
+// var num2  =num1++;
+
+// alert (num2)
+
+// var num = 5 ;
+// num++
+// alert (+ num)
+
+// CHAPTER 7
+
+// var calculateNum = 4+(2*6);
+// alert (calculateNum)
+// var calculatedNum = (2 + 6) * 6;
+// alert (calculatedNum)
+// var calculatedNum = (2 + 4) * (4 + 2);
+// alert (calculatedNum)
+
+// var calculatedNum = ((2 + 2) * 4) + 5;
+// alert (calculatedNum)
+
+// var coast = (2+2) * (4+10);
+// alert (coast)
+
+// var coast = 2+2 * 4+10 ;
+// alert (coast)
+
+// var coast =4/ (2*4);
+// alert (coast)
+
+// CHAPTER 8
+// var num = "2" + "2"
+// alert (num)
+
+// var msg = ("hello"+" moiz")
+// alert (msg)
+
+// var msg = ("33"+3)
+// alert (msg)
+
+// var msg = ("pakistan "+"zinddabad")
+// alert (msg)
+
+// var msg =("anus " + 122)
+// alert (msg)
+
+// var a = ("hello")
+// var b = ("boss")
+// var msg = a+b
+// alert (msg)
+
+// CHAPTER 9 
+
+// var a =prompt ("firstname")
+
+
+// var a = prompt ("Country?", "pakistan")
+
+// var  yourName =prompt ("Enter Your Name")
+
+
+// var name = prompt ("Name", "abddulmoiz")
+// alert ( name )
+
+// var message = "Enter your name:";
+// var defaultResponse = "John Doe";
+// var userResponse = prompt (message, defaultResponse)
+
+// var a = prompt ("Enter Your Name", "Anas ?");
+// alert (a);
+
+//  chapter 10
+
+//  var city = ("karachi")
+//  if (city="karachi") {
+//     console.log ("karachi the city of light")
+//  }
+
+// var x = "karachi";
+//  var y = "karachi";
+//  if (x === y){
+//      var z = prompt ("Enter Your CityName")
+//       }
+
+// var zipCode = "10010";
+// if (zipCode == "10010"){
+//     alert ("karachi");
+// }else {
+//     alert ("Please write correct city");
+// }
+
+// var x=1;
+
+// if(x===1){
+//     x=42;
+//     console.log("updated value of x:" + x);
+// }
